871159377f9c5028b9727e268e245244b5d8c50c
Waleed Ahmad <151533744+waleed-ahmd@users.noreply.github.com>
2024-12-16 15:31:14 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class MainTaxCalculations {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("====== Financial Calculator ======");
